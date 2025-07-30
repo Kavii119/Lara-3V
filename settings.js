7305e406d2c95b63256f51130137b6e50eba604d
Kavii119 <dilpahankaveesha5@gmail.com>
2025-07-30 13:12:30 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "XIxGAI5I#Kan0anzbrtZQJarAxGjNkKLs8zNB_xlFB7OOLUfLAXM", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "XUBzVKhJ#hI47_P3WxXbY6Kw9syic7RGm3ZmaYdZ3T4u4c2IDxbE", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 MODE: process.env.MODE || "public",  //public | private | inbox | group
 OWNER_NUMBER: process.env.OWNER_NUMBER || "94786130427",
